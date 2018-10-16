0db0720229e795e5caae53364b7ba7486aef24fa
Jason Amidan <jason.amidan@gmail.com>
2018-10-16 17:44:23 +0000

    Finished Example

--- bankApp.py
+++ bankApp.py
@@ -10,6 +10,11 @@ login_types = ["admin", "user", "guest"]
 def gatekeeper(login):
   if login == "admin":
     return "You have the privileges"
+  elif login=="user":
+    return "You have limited privileges"
+  elif login=="guest":
+    return "You have no privileges"
+
 
 # 3. Call the gatekeeper function with a string and print what it returns.
 print(gatekeeper("admin"))
@@ -18,9 +23,13 @@ print(gatekeeper("admin"))
 
 # 5. Complete the function called check_balance that takes one parameter, loan_balance.
 def check_balance(loan_balance):
+    if loan_balance>0:
+      return "You don't owe any money"
+    elif loan_balance<0:
+      return "You owe" + loan_balance
 # 6. If loan_balance is zero or more, it says “you don’t owe any money”
 # 7. If loan_balance is negative, it  says “you owe $X” where X is the amount
-    return
+
 
 # 8. Call your function with a negative and positive number and print what it returns.
 print(check_balance(300))
